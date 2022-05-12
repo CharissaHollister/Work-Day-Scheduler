5b0d629a59104af4a96bd4fc5bfa1d3df4d7be8d
CharissaHollister <charissa.hollister@diasorin.com>
2022-05-11 21:40:36 -0500

    pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,51 @@ var xxx = document.querySelector("#yyy");
 
 //---------define functions---------
 
+//function to clear local storage
+
+//if nothing in localStorage, create a new object to track all task status arrays
+
+//-------create elements------------
+//create header row 
+//create paragraph row 
+//create row for current date 
+//clear schedule button 
+// Create TimeBlocks in grid format
+    //create rows for each hour Block 9 to 5 with margins left and right
+        //col 1 hour block -1 column
+        //col 2 event info -10 columns
+        //col 3 save block -1 column
+
+//---------Date Input--------------
+//get date from local storage
+    //if date empty in local storage put in today's date
+    //if date is today continue
+    //if date in ls is the past ask user if they would like to 
+    //clear ls and continue with today OR keep old date and tasks
+
+//-----------color blocks--------
+    //figure out if want to run every 15 mins 
+//Color blocks based on current time
+    //past hours gray
+    //current hour red/pink
+    //future hours green
+
+//Edit Event Details
+    //make box editable
+    //update formatting to indicate it's in edit mode
+    //change from locked image to save button
+    //remove focus from box after saved
+    //click outside the box w/o save returns ls box content
+
+//save event details
+    //stores event data in ls
+    //changes button to locked image
+
+
+
+
+
+
 
 
 
@@ -31,10 +76,52 @@ var xxx = document.querySelector("#yyy");
 
 
 
-
-
-
 //---------------Add event listener-----------
-zzz.addEventListener('click', function(event){
 
-});
+//clicks button "clearSchedule"
+    //clear ls
+    //refresh the page anew
+
+//clicks button "Save"
+    //sends data from that row to ls
+    //return square to locked image
+
+//clicks event details
+    //make box editable
+
+//clicks save
+    //update event details for that row in ls
+
+
+
+
+
+
+
+
+
+    ////////////from task master sections//////////////////
+//audit task
+//create task
+//load task
+// if nothing in localStorage, create a new object to track all task status arrays
+//loop over object properties
+//loop over sub-array
+//save task
+// trim down list's ID to match object property
+//update array on tasks object and save
+//remove task
+//task text was clicked
+//get current text of p element
+//replace p element with new textarea
+//autofocus new element
+//editable field was un-focused
+//get current value of textarea
+//update task in array and re-save to localstorage
+//recreate p element
+//replace textarea with new content
+//set interval for refresh
+//load tasks for the first time
+
+
+
